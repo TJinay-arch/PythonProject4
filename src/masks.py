cee1d03d50a539da225a1ccb323f1f6d655dfec4
Ilya Payusov <ilya-payusov@mail.ru>
2025-08-02 15:09:28 +0300

    save a progress

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает номер кредитной карты и возвращает маску, скрывающую большую часть
+    номеров кроме первых трехи последнего блока."""
+    card_mask = card_number[0:4] + " " + card_number[5:7] + "** **** " + card_number[12:16]
+    return card_mask
+
+
+def get_mask_account(account: str) -> str:
+    """Функция принимает номер счета и возвращает его маску, скрывающую большую часть кроме
+    последних четырех символов."""
+    account_mask = "**" + account[-4:]
+    return account_mask
+
+
+
