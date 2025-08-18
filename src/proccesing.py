0a384c577651742105516148f16c1ff7d67f00a5
Ilya Payusov <ilya-payusov@mail.ru>
2025-08-18 17:33:03 +0300

    .\src\masks.py

--- src/proccesing.py
+++ src/proccesing.py
@@ -1,17 +1,13 @@
-def filter_by_state(data_list, state="EXECUTED"):
+def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Фильтрует список словарей по значению ключа 'state'.
-
-    :param data_list: Список словарей, каждый словарь содержит хотя бы один ключ 'state'.
-    :param state: Значение фильтра (опциональный аргумент), по умолчанию равно 'EXECUTED'.
-    :return: Новый список словарей, удовлетворяющих фильтру.
     """
     return [item for item in data_list if item.get("state") == state]
 
 
-def sort_by_date(data_list, reverse=True):
+def sort_by_date(data_list: list[dict], reverse: bool = True) -> list[dict]:
     """
     Сортирует список словарей по полю 'date'
     """
-    # Сортировка осуществляется прямо по значению поля 'date'
+
     return sorted(data_list, key=lambda item: item["date"], reverse=reverse)
